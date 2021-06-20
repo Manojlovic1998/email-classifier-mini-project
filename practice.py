d8e3e6a7e7b54b52866e343de5dbf3440e8b7124
Manojlovic1998 <manojlovic911@hotmail.com>
2021-06-20 05:43:49 +0200

    Create NB classifier using the data and print accuracy

--- practice.py
+++ practice.py
@@ -1,6 +1,16 @@
-import tarfile
+from tools import email_preprocess
+from sklearn.naive_bayes import GaussianNB
+
+words_file = "/word_data.pkl"
+email_authors = "/email_authors.pkl"
+features_train, features_test, labels_train, labels_test = email_preprocess.preprocess(words_file, email_authors)
 
 
-# Extraxt project files
-tfile = tarfile.open("enron_mail_20150507.tar.gz", "r:gz")
-tfile.extractall(".")
+# create classifier object
+clf = GaussianNB()
+
+
+# training the model
+clf.fit(features_train, labels_train)
+
+print(clf.score(features_test, labels_test))
